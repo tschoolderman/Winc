4c129e1c8add2be455dfae6965671381238bb9b8
tschoolderman <113764031+tschoolderman@users.noreply.github.com>
2022-09-26 19:25:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ scorer_two = 'Marco van Basten'
 goal_0 = 32
 goal_1 = 54
 
-scorers = (f'{scorer_one} {goal_0}, {scorer_two} {goal_1}')
+scorers = f'{scorer_one} {goal_0}, {scorer_two} {goal_1}'
 
-report = (f'{scorer_one} scored in the {goal_0}nd minute\n{scorer_two} scored in the {goal_1}th minute')
+report = f'{scorer_one} scored in the {goal_0}nd minute\n{scorer_two} scored in the {goal_1}th minute'
 
 player = 'Ronald Koeman'
-first_name = player[:6]
-last_name = player[7:]
+first_name = player[:player.find(' ')]
+last_name = player[player.find(' '):]
 last_name_len = len(last_name)
 name_short = player[0] + '. ' + last_name
 
 chant_0 = (first_name + '! ') * len(first_name)
 chant = chant_0[:-1]
-good_chant = chant != ' '
+good_chant = chant[-1] != ' '
